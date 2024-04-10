cdb41b8c67093cde41e24ed6d4b3bc312584d56e
MaryAntonisamy <48781850+MaryAntonisamy@users.noreply.github.com>
2024-04-10 06:10:59 +0100

    Create appsettings.cs

---
+++ appsettings.cs
@@ -0,0 +1,15 @@
+{
+  "CosmosDb": {
+    "EndpointUri": "<Your_Cosmos_DB_URI>",
+    "PrimaryKey": "<Your_Cosmos_DB_Primary_Key>",
+    "DatabaseId": "<Your_Database_Id>",
+    "ContainerId": "<Your_Container_Id>"
+  },
+  "ServiceBus": {
+    "ConnectionString": "<Your_Service_Bus_Connection_String>",
+    "QueueName": "<Your_Queue_Name>"
+  },
+  "Schedule": {
+    "IntervalInMinutes": 30
+  }
+}
